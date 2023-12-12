08a73f568520dee2e2194eeb9b59bb4072d95036
NivedHari <nivedhari44@gmail.com>
2023-12-12 20:38:48 +0530

    Making Components Smaller and Readable

---
+++ src/components/ExpenseDetails.js
@@ -0,0 +1,8 @@
+function ExpenseDetails(props) {
+  return (
+  <div className="expense-item__price">
+  {props.amount}
+  </div>);
+}
+
+export default ExpenseDetails;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,16 @@
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+import ExpenseDetails from "./ExpenseDetails";
 
 function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
+      <ExpenseDate date={props.date} />
       <div>{props.location}</div>
       <div className="expense__description">
         <h2>{props.title}</h2>
       </div>
-      <div className="expense-item__price">${props.amount}</div>
+      <ExpenseDetails amount={props.amount}/>
     </div>
   );
 }
