fa564cc7f41fbb1d5d0dbe4e2a2e82246d74205b
NivedHari <nivedhari44@gmail.com>
2023-12-12 15:17:02 +0530

    LocationOfExpenditure

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,21 @@
-function ExpenseItem() {
-    return (
-        <div>
-            <h2>Expense Items</h2>
-            <p>Food Rs 10</p>
-            <p>Petrol Rs 100</p>
-            <p>Movies Rs 200</p>
-        </div>
+import "./ExpenseItem.css";
 
-    )
-    
+function ExpenseItem() {
+  const expenseDate = new Date(2023, 11, 12);
+  const expenseTitle = "Car Insurence";
+  const expenseAmount = 295.45;
+  const LocationOfExpenditure = 'Canada';
+
+  return (
+    <div className="expense-item">
+      <div>{expenseDate.toISOString()}</div>
+      <div>{LocationOfExpenditure}</div>
+      <div className="expense__description">
+        <h2>{expenseTitle}</h2>
+      </div>
+      <div className="expense-item__price">${expenseAmount}</div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
