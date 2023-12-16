33da3d767b2dcd297b461c4accd75d41266480d5
NivedHari <nivedhari44@gmail.com>
2023-12-16 15:49:50 +0530

    Filter implementation

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -12,13 +12,19 @@ const Expenses = (props) => {
     setFilteredYear(selectedYear);
   };
 
+  const filteredExpenses = items.filter((expense) =>{
+    const expenseDate = new Date(expense.date);
+
+    return expenseDate.getFullYear().toString() === filteredYear;
+  })
+
   return (
     <Card className="expenses-container">
       <ExpensesFilter
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-      {items.map((expense) => (
+      {filteredExpenses.map((expense) => (
         <ExpenseItem
           key={expense.id}
           title={expense.title}
