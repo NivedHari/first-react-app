38d48e3c3fc6b27890f05fcdee4678943949e670
NivedHari <nivedhari44@gmail.com>
2023-12-16 17:02:22 +0530

    Conditional Rendering

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,38 +1,31 @@
 import React, { useState } from "react";
-import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
 
 const Expenses = (props) => {
-  const { items } = props;
-
   const [filteredYear, setFilteredYear] = useState("2020");
 
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
 
-  const filteredExpenses = items.filter((expense) =>{
+  const filteredExpenses = props.items.filter((expense) => {
     const expenseDate = new Date(expense.date);
 
     return expenseDate.getFullYear().toString() === filteredYear;
-  })
+  });
 
   return (
+    <li>
     <Card className="expenses-container">
       <ExpensesFilter
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-      {filteredExpenses.map((expense) => (
-        <ExpenseItem
-          key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
-      ))}
+      <ExpensesList items={filteredExpenses} />
     </Card>
+    </li>
   );
 };
 
